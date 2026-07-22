af7cbbbe7f09c7e5e461348330c73219e7be5c54
jedkutai <jedkutai@gmail.com>
2026-07-22 16:31:46 -0400

    add uart logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-//! Blink the built-in LED using the SysTick timer.
+//! Blink the built-in LED and send UART log messages.
 
 #![deny(unsafe_code)]
 #![allow(clippy::empty_loop)]
 #![no_main]
 #![no_std]
+
+use core::fmt::Write;
 
 use panic_halt as _;
 
@@ -23,15 +25,33 @@ fn main() -> ! {
         // Run the system clock at 48 MHz using the internal oscillator.
         let mut rcc = dp.RCC.freeze(Config::hsi().sysclk(48.MHz()));
 
-        // The Black Pill's built-in LED is connected to PC13.
+        // Built-in LED on pin C13.
         let gpioc = dp.GPIOC.split(&mut rcc);
         let mut led = gpioc.pc13.into_push_pull_output();
 
-        // Create a blocking delay using the Cortex-M SysTick timer.
+        // UART transmit pin.
+        let gpioa = dp.GPIOA.split(&mut rcc);
+        let tx_pin = gpioa.pa9;
+
+        // Configure USART1 at 115200 baud.
+        let mut uart = dp
+            .USART1
+            .tx(tx_pin, 115_200.bps(), &mut rcc)
+            .unwrap();
+
+        // Delay using the Cortex-M SysTick timer.
         let mut delay = cp.SYST.delay(&rcc.clocks);
+
+        let mut heartbeat: u32 = 0;
+
+        writeln!(uart, "Garage monitor starting...\r").ok();
 
         loop {
             led.toggle();
+
+            writeln!(uart, "heartbeat={heartbeat}\r").ok();
+            heartbeat = heartbeat.wrapping_add(1);
+
             delay.delay_ms(1000);
         }
     }
